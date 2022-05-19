008f5315a9fe7b44122f0170bb96bfd63de32dd2
maar20102 <49079956+maar20102@users.noreply.github.com>
2022-05-19 20:04:59 +0300

    update score for player

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ choices = ["Rock", "Paper", "Scissors"]
 player = False
 cpu_score = 0
 player_score = 0
+count =          0
 while True:
     computer = random.choice(choices)
 
@@ -13,24 +14,39 @@ while True:
     elif player == "Rock":
         if computer == "Paper":
             print("You lose!", computer, "covers", player)
+            count =0
             cpu_score+=1
         else:
             print("You win!", player, "smashes", computer)
-            player_score+=1
+            count +=1
+            if count == 3:
+                player_score+=4
+            else:
+                player_score+=1
     elif player == "Paper":
         if computer == "Scissors":
             print("You lose!", computer, "cut", player)
+            count =0
             cpu_score+=1
         else:
             print("You win!", player, "covers", computer)
-            player_score+=1
+            count +=1
+            if count == 3:
+                player_score+=4
+            else:
+                player_score+=1
     elif player == "Scissors":
         if computer == "Rock":
             print("You lose...", computer, "smashes", player)
+            count = 0
             cpu_score+=1
         else:
             print("You win!", player, "cut", computer)
-            player_score+=1
+            count +=1
+            if count == 3:
+                player_score+=4
+            else:
+                player_score+=1
     elif player=='End':
         print("Final Scores:")
         print(f"CPU:{cpu_score}")
